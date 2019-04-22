d89c3ec3dfcdafbbb6b219aeb4a225dcd14b0ba1
wes <weshenshall@gmail.com>
2019-04-21 22:34:29 -0400

    added validator and converted data file

--- converter.php
+++ converter.php
@@ -1,14 +1,35 @@
 <?php
 
+// NOTE ON THE CONVERTER
+
+// This function needs to accept a signed integer in the 14-bit range [-8192..+8191] and return a 4 character
+// string.
+// The encoding process is as follows:
+// 1. Add 8192 to the raw value, so its range is translated to [0..16383]
+// 2. Pack that value into two bytes such that the most significant bit of each is cleared
+// Unencoded intermediate value (as a 16-bit integer):
+//   3. Format the two bytes as a single 4-character hexadecimal string and return it.
+//
+
+// EXAMPLES:
+// 0 should encode to 4000
+// -8192 should encode to 0000
+// 8191 should encode to 7F7F
+// ETC.
+
+
 class Converter
 {
   public static function encode($num){
-
+    // validate input
+    if ($num < -8192 || $num > 8191) {
+      return "you need to input a number between -8192 and 8191";
+    }
     // add 8192
     $num = $num + 8192;
     // get binary
     $binary = decbin($num);
-    echo $binary;
+    // echo $binary;
     // assign two "bytes" to each of the halves of the binary (since $num will always be 14 binary digets long, we can seperate them into 2 halves)
     $byte1 = $num >> 7;
     $byte2 = $binary & $byte1;
@@ -56,16 +77,14 @@ class Converter
     $times = 7 - (int)strlen(strval($lo)) ;
     for ($i=0; $i < $times; $i++) {
       $lo = "0" . $lo;
-      echo " i = " . $i .   " lo = " . $lo . "\n";
+      // echo " i = " . $i .   " lo = " . $lo . "\n";
     }
 
-
-
-    echo $hi .  "\n";
-    echo $lo .  "\n";
+    // echo $hi .  "\n";
+    // echo $lo .  "\n";
     $binary = $hi . $lo;
 
-    echo $binary.  "\n";
+    // echo $binary.  "\n";
 
     $num = bindec((int)$binary);
 
@@ -75,12 +94,8 @@ class Converter
 
 }
 
-echo "encode returns === " .  Converter::encode(2048) . "\n";
-echo "decode returns === " .  Converter::decode("20", "00") . "\n";
-
-
-
-
+echo "encode returns === " .  Converter::encode(340) . "\n";
+echo "decode returns === " .  Converter::decode("0A", "0A") . "\n";
 
 
 
